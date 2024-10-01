806f6049a3568ccbadd3ccd83d2951182f2694c6
Joshua K John <joshuakjohn7@gmail.com>
2024-10-01 10:38:50 +0530

    [joshua] add - added feature for finding and Editing an Existing Contact

--- main.js
+++ main.js
@@ -62,3 +62,15 @@ try {
     console.error(error.message);
 }
 
+AddressBook.prototype.findContact = function (firstName, lastName) {
+    return this.contacts.find(contact => contact.firstName === firstName && contact.lastName === lastName);
+};
+
+AddressBook.prototype.editContact = function (firstName, lastName, newDetails) {
+    const contact = this.findContact(firstName, lastName);
+    if (contact) {
+        Object.assign(contact, newDetails);
+    } else {
+        console.log('Contact not found');
+    }
+};
