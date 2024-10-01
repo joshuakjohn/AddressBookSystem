09b4a228b41827f5a2554a323b6bf5430052904f
Joshua K John <joshuakjohn7@gmail.com>
2024-10-01 10:43:17 +0530

    [joshua] add - added feature to checkout for duplicates

--- main.js
+++ main.js
@@ -82,3 +82,15 @@ AddressBook.prototype.deleteContact = function (firstName, lastName) {
 AddressBook.prototype.countContacts = function () {
     return this.contacts.reduce((count) => count + 1, 0);
 };
+
+AddressBook.prototype.hasDuplicate = function (firstName, lastName) {
+    return this.contacts.some(contact => contact.firstName === firstName && contact.lastName === lastName);
+};
+
+AddressBook.prototype.addUniqueContact = function (contact) {
+    if (!this.hasDuplicate(contact.firstName, contact.lastName)) {
+        this.addContact(contact);
+    } else {
+        console.log('Duplicate entry found');
+    }
+};
